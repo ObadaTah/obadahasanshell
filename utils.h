2c0db3726b7636e81a0cb2677e5e3081980e3389
ObadaTah <obadatahboub20@gmail.com>
2025-04-07 22:16:54 +0300

    ASdfasfsdg

--- utils.h
+++ utils.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTIONS_H_INCLUDED
-#define FUNCTIONS_H_INCLUDED
+#ifndef UTILS_H_INCLUDED
+#define UTILS_H_INCLUDED
 
 // --- Function Prototypes ---
 char *read_input_line();
@@ -8,11 +8,5 @@ int parse_command_args(char *command, char **args);
 void execute_command(char **args);
 int handle_cd_command(char **args, int num_args);
 void print_prompt(); // New function to print the prompt
-
-/* ^^ these are the include guards */
-
-/* Prototypes for the functions */
-/* Sums two ints */
-int Sum(int a, int b);
 
 #endif
